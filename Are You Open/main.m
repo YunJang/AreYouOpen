ba459a0cd02f3544f2ac25dd29314453fafde158
Yun Jang <yunjang@Yuns-MacBook-Pro.local>
2014-07-12 23:58:06 -0500

    Initial Commit

---
+++ Are You Open/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Are You Open
+//
+//  Created by Yun Jang on 7/12/14.
+//  Copyright (c) 2014 Yun Jang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
